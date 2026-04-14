718b425f06d4336ecbe46678a2c0dce3033cce06
Nawin chander <nawinnapzy289@gmail.com>
2026-04-14 18:58:28 +0530

    add

--- User Service/client.js
+++ User Service/client.js
@@ -1,2 +1,12 @@
 /// client
 
+const client = new userPackage.UserService(
+  "localhost:50051",
+  grpc.credentials.createInsecure()
+);
+
+client.GetUser({ id: "1" }, (err, response) => {
+  console.log(response);
+});
+
+
