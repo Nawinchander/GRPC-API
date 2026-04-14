26aab1a05df7a5f2701ada83a4cdc88a9d3ea70f
Nawin chander <nawinnapzy289@gmail.com>
2026-04-14 19:00:23 +0530

    add

--- Streaming API/Server.js
+++ Streaming API/Server.js
@@ -1,1 +1,11 @@
 /// server side
+
+function getNotifications(call) {
+  const messages = ["Hello", "New Message", "System Alert"];
+  messages.forEach(msg => {
+    call.write({ message: msg });
+  });
+  call.end();
+}
+
+
