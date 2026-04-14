12303909be86d65b27ac062c97b50a3cd2fbf1f7
Nawin chander <nawinnapzy289@gmail.com>
2026-04-14 18:57:52 +0530

    add

--- User Service/server.js
+++ User Service/server.js
@@ -1,1 +1,21 @@
 /// server
+
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+
+const packageDef = protoLoader.loadSync("user.proto");
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const userPackage = grpcObject;
+
+function getUser(call, callback) {
+  callback(null, { id: call.request.id, name: "Nawin" });
+}
+
+const server = new grpc.Server();
+server.addService(userPackage.UserService.service, { GetUser: getUser });
+
+server.bindAsync("0.0.0.0:50051", grpc.ServerCredentials.createInsecure(), () => {
+  server.start();
+});
+
+
